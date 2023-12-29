566a3b144e91a6360a20760a65b4a9d4a1f1f429
-T.K.- <t_k_233@outlook.com>
2023-12-29 00:36:02 -0800

    ADD: upgrade to use datetime versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="cc.serializer",
-    version="0.0.4",
+    version="2023.12.26",
     author="Uncertainty.",
     author_email="t_k_233@outlook.email",
     description="Transmit and receive Serial data and divide the stream into packets with NLSM protocol.",
@@ -25,5 +25,5 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    python_requires=">=3.8",
 )
